a06e0d1fcc496b43e4bb0d8f0b80c9a5e612305f
Tim Maffett <timmaffett@gmail.com>
2023-02-23 11:03:12 -0800

    more cleanup of other pr

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_font_picker/flutter_font_picker.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,33 +29,10 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-enum FontListType { subset, complete }
 class _MyHomePageState extends State<MyHomePage> {
-  String _selectedFont = 'Roboto';
-
-  late TextStyle? _selectedFontTextStyle = GoogleFonts.getFont(_selectedFont);
-
-  bool _showFontVariants = true;
-
-  bool _showFontInfo = true;
-
-  bool _showListPreviewSampleTextInput = false;
-
-  String _listPreviewSampleText = '';
-
-  double _sampleTextFontSize = 14.0;
-
-  double _fontPickerListFontSize = 16.0;
-
-  double _previewFontSize = 24.0;
-
-  FontListType _fontListType = FontListType.subset;
-
-  late List<String> _myGoogleFonts = _subsetListOfGoogleFonts;
-
-  final List<String> _completeGoogleFonts = GoogleFonts.asMap().keys.toList();
-
-  final List<String> _subsetListOfGoogleFonts = [
+  String _selectedFont = "Roboto";
+  TextStyle? _selectedFontTextStyle;
+  final List<String> _myGoogleFonts = [
     "Abril Fatface",
     "Aclonica",
     "Alegreya Sans",
@@ -110,18 +86,6 @@ class _MyHomePageState extends State<MyHomePage> {
     "Work Sans",
     "Zilla Slab",
   ];
-
-  void _onFontListTypeChange( FontListType? val ) {
-    setState(() {
-      _fontListType = val ?? FontListType.subset;
-      if(_fontListType == FontListType.subset) {
-        _myGoogleFonts = _subsetListOfGoogleFonts;
-      } else {
-        _myGoogleFonts = _completeGoogleFonts;
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -134,175 +98,6 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'List of fonts is :',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  Radio<FontListType>(
-                    value: FontListType.subset,
-                    groupValue: _fontListType,
-                    onChanged: _onFontListTypeChange,
-                  ),
-                  const Text(
-                    'Subset of GoogleFonts',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  Radio<FontListType>(
-                    value: FontListType.complete,
-                    groupValue: _fontListType,
-                    onChanged: _onFontListTypeChange,
-                  ),
-                  const Text(
-                    'All GoogleFonts',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Show font variants :',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Checkbox(
-                    value: _showFontVariants,
-                    onChanged: (checked) {
-                      setState(() {
-                        _showFontVariants = checked ?? false;
-                      });
-                    },
-                  ),
-                  const SizedBox(width: 30),
-                  const Text(
-                    'Show font info :',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Checkbox(
-                    value: _showFontInfo,
-                    onChanged: (checked) {
-                      setState(() {
-                        _showFontInfo = checked ?? false;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded( child: FontSample(
-                              onSampleTextChanged: (newSample) {
-                                setState(() {
-                                  _listPreviewSampleText = newSample;
-                                });
-                              },
-                            ),
-                  ),
-                  const SizedBox(width: 30),
-                  const Text(
-                    'Allow editing of list preview sample text :',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Checkbox(
-                    value: _showListPreviewSampleTextInput,
-                    onChanged: (checked) {
-                      setState(() {
-                        _showListPreviewSampleTextInput = checked ?? false;
-                      });
-                    },
-                  ),
-                ],
-              ),
-
-
-
-
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Font size for sample text :',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  Slider(
-                    min: 10.0,
-                    max: 64.0,
-                    value: _sampleTextFontSize,
-                    onChanged: (value) {
-                      setState(() {
-                        _sampleTextFontSize = value.round().toDouble();
-                      });
-                    },
-                  ),
-                  Text(
-                    '${_sampleTextFontSize}px',
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Font size for fonts in picker list :',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  Slider(
-                    min: 10.0,
-                    max: 64.0,
-                    value: _fontPickerListFontSize,
-                    onChanged: (value) {
-                      setState(() {
-                        _fontPickerListFontSize = value.round().toDouble();
-                      });
-                    },
-                  ),
-                  Text(
-                    '${_fontPickerListFontSize}px',
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child:Container( height: 2, color: Colors.black ),
-              ),
-              const Text(
-                'Examples of FontPicker() (using above settings):',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16.0,
-                ),
-              ),
-              SizedBox(height: 12),
               ElevatedButton(
                 child: const Text('Pick a font (with a screen)'),
                 onPressed: () {
@@ -321,19 +116,11 @@ class _MyHomePageState extends State<MyHomePage> {
                           );
                         },
                         googleFonts: _myGoogleFonts,
-                        showFontVariants: _showFontVariants,
-                        showFontInfo: _showFontInfo,
-                        showListPreviewSampleTextInput: _showListPreviewSampleTextInput,
-                        listPreviewSampleText: _listPreviewSampleText,
-                        previewSampleTextFontSize: _sampleTextFontSize,
-                        fontSizeForListPreview: _fontPickerListFontSize,
-
                       ),
                     ),
                   );
                 },
               ),
-              const SizedBox(height:16),
               ElevatedButton(
                 child: const Text('Pick a font (with a dialog)'),
                 onPressed: () {
@@ -357,12 +144,6 @@ class _MyHomePageState extends State<MyHomePage> {
                                 );
                               },
                               googleFonts: _myGoogleFonts,
-                              showFontVariants: _showFontVariants,
-                              showFontInfo: _showFontInfo,
-                              showListPreviewSampleTextInput: _showListPreviewSampleTextInput,
-                              listPreviewSampleText: _listPreviewSampleText,
-                              previewSampleTextFontSize: _sampleTextFontSize,
-                              fontSizeForListPreview: _fontPickerListFontSize,
                             ),
                           ),
                         ),
@@ -389,7 +170,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: TextField(
                       readOnly: true,
                       textAlign: TextAlign.center,
-                      style: _selectedFontTextStyle?.copyWith(fontSize:_fontPickerListFontSize),
                       decoration: InputDecoration(
                         suffixIcon: const Icon(Icons.arrow_drop_down_sharp),
                         hintText: _selectedFont,
@@ -410,47 +190,11 @@ class _MyHomePageState extends State<MyHomePage> {
                                 );
                               },
                               googleFonts: _myGoogleFonts,
-                              showFontVariants: _showFontVariants,
-                              showFontInfo: _showFontInfo,
-                              showListPreviewSampleTextInput: _showListPreviewSampleTextInput,
-                              listPreviewSampleText: _listPreviewSampleText,
-                              previewSampleTextFontSize: _sampleTextFontSize,
-                              fontSizeForListPreview: _fontPickerListFontSize,
                             ),
                           ),
                         );
                       },
                     ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child:Container( height: 2, color: Colors.black ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Preview font size :',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  Slider(
-                    min: 10.0,
-                    max: 96.0,
-                    value: _previewFontSize,
-                    onChanged: (value) {
-                      setState(() {
-                        _previewFontSize = value.round().toDouble();
-                      });
-                    },
-                  ),
-                  Text(
-                    '${_previewFontSize}px',
-                    style: const TextStyle(fontSize: 16.0),
                   ),
                 ],
               ),
@@ -466,34 +210,23 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     child: Padding(
                       padding: const EdgeInsets.all(12.0),
-                      child: ClipRect(
-                        clipBehavior: Clip.hardEdge,
-                        child: OverflowBox(
-                          alignment: Alignment.center,
-                          minWidth: 0.0,
-                          minHeight: 0.0,
-                          maxWidth: double.infinity,
-                          maxHeight: double.infinity,   
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Font: $_selectedFont',
-                                style: _selectedFontTextStyle?.copyWith(fontSize:_previewFontSize),
-                                textAlign: TextAlign.center,
-                              ),
-                              Text(
-                                'The quick brown fox jumped',
-                                style: _selectedFontTextStyle?.copyWith(fontSize:_previewFontSize),
-                                textAlign: TextAlign.center,
-                              ),
-                              Text(
-                                'over the lazy dog',
-                                style: _selectedFontTextStyle?.copyWith(fontSize:_previewFontSize),
-                                textAlign: TextAlign.center,
-                              ),
-                            ],
-                          ),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Font: $_selectedFont',
+                              style: _selectedFontTextStyle,
+                            ),
+                            Text(
+                              'The quick brown fox jumped',
+                              style: _selectedFontTextStyle,
+                            ),
+                            Text(
+                              'over the lazy dog',
+                              style: _selectedFontTextStyle,
+                            ),
+                          ],
                         ),
                       ),
                     ),
@@ -502,74 +235,6 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-
-
-class FontSample extends StatefulWidget {
-  final String initialSampleText;
-  final ValueChanged<String> onSampleTextChanged;
-  const FontSample({super.key, this.initialSampleText = '', required this.onSampleTextChanged});
-
-  @override
-  _FontSampleState createState() => _FontSampleState();
-}
-
-class _FontSampleState extends State<FontSample> {
-  bool _isSampleFocused = false;
-  late final TextEditingController sampleController;
-
-  @override
-  void initState() {
-    super.initState();
-    sampleController = TextEditingController(text:widget.initialSampleText);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FocusScope(
-      child: Focus(
-        onFocusChange: (focus) {
-          setState(() {
-            _isSampleFocused = focus;
-          });
-        },
-        child: TextFormField(
-          controller: sampleController,
-          decoration: InputDecoration(
-            prefixIcon: const Icon(
-              Icons.list,
-            ),
-            label:  const Text(
-                    'List preview sample text',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                    ),
-                  ),
-            suffixIcon: _isSampleFocused
-                ? IconButton(
-                    icon: const Icon(Icons.cancel),
-                    onPressed: () {
-                      FocusScope.of(context).unfocus();
-                      sampleController.clear();
-                      widget.onSampleTextChanged('');
-                    },
-                  )
-                : null,
-            hintText: 'Optional sample text to add to each font preview in list',
-            hintStyle: const TextStyle(fontSize: 14.0),
-            border: InputBorder.none,
-            focusedBorder: InputBorder.none,
-            enabledBorder: InputBorder.none,
-            errorBorder: InputBorder.none,
-            disabledBorder: InputBorder.none,
-          ),
-          onChanged: widget.onSampleTextChanged,
         ),
       ),
     );
